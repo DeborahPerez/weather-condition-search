cb24305aa1372cb943418d64a1b564334819bf99
Deborah Perez <perezdeboraha@gmail.com>
2019-06-20 16:00:24 -0400

    modified to query based on longitude and latitude

--- weather_api.py
+++ weather_api.py
@@ -3,9 +3,10 @@ import requests
 from openweathermap_token import APPID
 
 api_token = '&APPID=' + APPID
-api_url_base = "http://api.openweathermap.org/data/2.5/weather?id="
+api_url_base = "http://api.openweathermap.org/data/2.5/weather?"
 json_city_ids = 'city.list.json'
-
+lat = '40.71'
+lon = '-74.01'
 headers = {'Content-Type': 'application/json',
 	   'Authorization': 'Bearer {0}'.format(api_token)}
 
@@ -14,12 +15,13 @@ headers = {'Content-Type': 'application/json',
 def read_json_file (jsonFile):
     with open(jsonFile, 'r', encoding="latin-1") as fin:
         data = json.load(fin)
+#        print (data[0])
         return data
 
 
 # Test capturing data
 def test_city_info():
-    api_url = '{0}5128581{1}'.format(api_url_base, api_token)
+    api_url = '{0}lat={1}&lon={2}{3}'.format(api_url_base, lat, lon, api_token)
 
     response = requests.get(api_url, headers=headers)
 
@@ -32,7 +34,7 @@ def test_city_info():
 account_info = test_city_info()
 
 if account_info is not None:
-    print ("Here's your info: ")
+    print ("Here's your info: ", account_info)
     name = account_info['name']
     weatherCondition = account_info['weather'][0]['main']
     temp_K = int(account_info['main']['temp'])
@@ -44,3 +46,8 @@ else:
 
 # Read city json file
 city_data = read_json_file(json_city_ids)
+us_city_count = 0
+for data in city_data:
+    if data['country'] == "US":
+        us_city_count += 1
+print ("US city count is: ", us_city_count)
