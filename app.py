c4d6bbd7f5b2eef64ce2485d104fc2b913aadafa
Deborah Perez <perezdeboraha@gmail.com>
2019-06-20 20:36:46 -0400

    Application takes in condition and relays results to be displayed on the webpage

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 import os
-from flask import Flask, render_template
-
-
+from flask import Flask, render_template, request
+import weather_api
+from condition_types import conditions
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 print (os.environ['APP_SETTINGS'])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+    results = []
+    if request.method == "POST":
+        # get weather condition that the person has entered
+        try:
+            condition = request.form['condition']
+            if condition.lower() in conditions:
+                results = weather_api.get_cities_for_condition(condition)
+            else:
+                errors.append("Not an acceptable weather condition")
+                return render_template('index.html', errors=errors)
+
+        except:
+            errors.append("Unable to get condition. Please try again!")
+            return render_template('index.html', errors=errors)
+
+    return render_template('index.html', errors=errors, results=results)
 
 if __name__ == "__main__":
     app.run()
